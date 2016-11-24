a88a5c67f09e03ef0edbf1f26d0c97497fb7eb67
kernel beau <kernelbeau@gmail.com>
2016-11-23 20:35:57 -0500

    commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
+setup(name='Y-town',
+      version='0.1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='kernel beau',
+      author_email='kernelbeau@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
